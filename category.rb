e60b784cdb5f5d8495f2df733bd10552da038ac8
Susan Cisneros <cisnerss@rose-hulman.edu>
2012-01-11 18:42:29 -0500

    implemented category

--- category.rb
+++ category.rb
@@ -5,4 +5,13 @@ include 'attribute.rb'
 class Category
   include DataMapper::Resource
   
+  property :id,   Serial
+  property :name, String
+  
+  has n, :categoryattributes
+  
+  def add_attribute(key)
+    @categoryattributes << CategoryAttribute.create(key)
+  end
+  
 end
