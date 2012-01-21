bc4f2950fd17a8ba56e8a083994aefd99546bfe2
Richard Thai <richardpthai@gmail.com>
2012-01-21 15:11:47 -0500

    Added testing class for Item, to be used as an example for future tests

--- dm.test.rb
+++ dm.test.rb
@@ -35,4 +35,59 @@ describe Item, "#upc" do
   it "returns 25 for default upc" do
     itm.upc.should eq(25)
   end
+  it "returns 50 for updated upc" do
+    itm.upc = 50
+    itm.save
+    itm.upc.should eq(50)
+  end
+end
+
+describe Item, "#iname" do
+  it "returns 'RAM 8' for default iname" do
+    itm.iname.should eq("RAM 8")
+  end
+  it "returns 'RAM 16' for updated iname" do
+    itm.iname = 'RAM 16'
+    itm.save
+    itm.iname.should eq('RAM 16')
+  end
+end
+
+describe Item, "#location" do
+  it "returns 'box 4 in the basement' for default location" do
+    itm.location.should eq('box 4 in the basement')
+  end
+  it "returns 'box 10 in the basement' for updated location" do
+    itm.location = 'box 10 in the basement'
+    itm.save
+    itm.location.should eq('box 10 in the basement')
+  end
+end
+
+describe Item, "#notes" do
+  it "returns nil for default notes" do
+    itm.notes.should eq(nil)
+  end
+  it "returns 'foo' for updated upc" do
+    itm.notes = 'foo'
+    itm.save
+    itm.notes.should eq('foo')
+  end
+end
+
+describe Item, "#date_added" do
+  it "returns noon at the new year (2012) for the default date added" do
+    itm.date_added.should eq(dt)
+  end
+end
+
+describe Item, "#date_modified" do
+  it "returns noon at the new year (2012) for the default date modified" do
+    itm.date_modified.should eq(dt)
+  end
+  it "returns 4:30 PM at the 25th of January of 2012 for the updated date modified" do
+    itm.date_modified = dt2
+    itm.save
+    itm.date_modified.should eq(dt2)
+  end
 end
