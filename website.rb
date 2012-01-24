38b782ec5877fc17ba74f69f12afd748c206fbe7
Richard Thai <richardpthai@gmail.com>
2012-01-24 16:42:00 -0500

    Added slight documentation to run the website.rb file.

--- website.rb
+++ website.rb
@@ -2,6 +2,14 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+=begin
+  Quick start:
+    $ruby website.rb
+    
+  Open the browser and go to localhost:xxxx where xxxx is given in the terminal.
+  Need to restart the website every time a change is made.
+=end
+
 get '/deeper' do
   "This is deeper Sinatra."
 end
