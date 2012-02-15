48bbfc08e007f3eab111affdf690e728f1eb709b
purviatt <purviatt@ubuntu.ubuntu-domain>
2012-02-15 17:15:46 -0500

    Details page can show details

---
+++ routes/AddItemUI.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+class AddItemUI
+	get '/additem' do
+    "add item page"
+  end
+end
--- routes/details.rb
+++ routes/details.rb
@@ -1,5 +1,8 @@
 class PersonalInventoryManager
   get '/details' do
+    upc = params[:upc].to_i  
+    @detail_item = Item.first(:upc => upc)
+
     haml :details
   end
-end
+end 
